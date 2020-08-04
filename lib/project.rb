c995eeeb7f59bd4f917594b3b5b6f2596373cb61
Andy Adams <andrewdcadams@gmail.com>
2020-08-04 20:41:32 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+class Project 
+  attr_accessor :title
+  attr_reader :backed_projects
+  
+  def initialize(title)
+    @title = title 
+    @backed_projects = []
+  end
+  
+  
+  
+  
+  
+  
+  
+end 
